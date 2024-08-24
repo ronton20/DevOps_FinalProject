72f334eba0b81813b5f850bee2ade982d66f986e
Ron Ariel <89269663+ronton20@users.noreply.github.com>
2024-08-24 18:23:42 +0300

    added docker files

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ from resources.item import Item, ItemList, items_ns, item_ns
 from marshmallow import ValidationError
 
 app = Flask(__name__)
-bluePrint = Blueprint('api', __name__, url_prefix='/api')
-api = Api(bluePrint, doc='/doc', title='Sample Flask-RestPlus Application')
+bluePrint = Blueprint("api", __name__, url_prefix="/api")
+api = Api(bluePrint, doc="/doc", title="Sample Flask-RestPlus Application")
 app.register_blueprint(bluePrint)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
 
 api.add_namespace(item_ns)
 api.add_namespace(items_ns)
@@ -32,12 +32,12 @@ def handle_validation_error(error):
     return jsonify(error.messages), 400
 
 
-item_ns.add_resource(Item, '/<int:id>')
+item_ns.add_resource(Item, "/<int:id>")
 items_ns.add_resource(ItemList, "")
-store_ns.add_resource(Store, '/<int:id>')
+store_ns.add_resource(Store, "/<int:id>")
 stores_ns.add_resource(StoreList, "")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     db.init_app(app)
     ma.init_app(app)
-    app.run(port=5000, debug=True,host='0.0.0.0')
+    app.run(port=5000, debug=True, host="0.0.0.0")
